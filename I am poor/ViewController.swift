1eb9151489dad5b6c95542ae4d15c128491686c7
muneebGH <muneeb.github@gmail.com>
2019-12-17 16:20:47 +0500

    Initial Commit

---
+++ I am poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I am poor
+//
+//  Created by Muneeb Ur Rehman on 17/12/2019.
+//  Copyright © 2019 Muneeb Ur Rehman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
